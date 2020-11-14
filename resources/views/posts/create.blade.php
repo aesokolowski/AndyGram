45a7a9b110cfe1dc04c4f693bb40d8a9478af386
Andy Sokolowski <44684409+aesokolowski@users.noreply.github.com>
2020-11-14 17:23:05 -0500

    feat: p/create has a rudmentary form entry field now.

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,6 +2,28 @@
 
 @section('content')
 <div class="container">
-   
+    <form action="">
+        <div class="row">
+            <div class="col-8 offset-2">
+                <div class="form-group row">
+		    <label for="caption" class="col-md-4 col-form-label">
+			Post Caption
+		    </label>
+        	    <input id="caption"
+			   type="text"
+			   class="form-control @error('caption') is-invalid @enderror"
+			   name="caption"
+													   value="{{ old('caption') }}"
+			   autocomplete="caption"
+			   autofocus />
+		    @error('caption')
+		    <span class="invalid-feedback" role="alert">
+			<strong>{{ $message }}</strong>
+		    </span>
+		    @enderror
+		</div>
+	    </div>
+	</div>
+    </form>
 </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/p', [App\Http\Controllers\PostsController::class, 'create']);
+Route::get('/p/create', [App\Http\Controllers\PostsController::class, 'create']);
 
 Route::get('/profile/{user}', [App\Http\Controllers\ProfilesController::class,
         'index'])->name('profile.show');

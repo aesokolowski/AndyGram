b6d665dbf0975812685b63f56cb3c59d3b5f91cb
Andy Sokolowski <44684409+aesokolowski@users.noreply.github.com>
2020-11-14 17:41:13 -0500

    ui: post image field added to p/create form

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -22,8 +22,22 @@
 		    </span>
 		    @enderror
 		</div>
-	    </div>
-	</div>
+                <div class="row">
+                    <label for="image" class="col-md-4 col-form-label">
+		        Post Image
+	            </label>
+                    <input id="image"
+                           type="file"
+                           class="form-control-file @error('image') is-invalid @enderror"
+                           name="image" />
+                    @error('image');
+	            <span class="invalid-feedback" role="alert">
+		        <strong>{{ $message }}</strong>
+	            </span>
+	            @enderror
+                </div>
+            </div>
+        </div>
     </form>
 </div>
 @endsection

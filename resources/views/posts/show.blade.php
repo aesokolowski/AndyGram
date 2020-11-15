76245175b5583b7f35785911be827d4c7bbbae52
Andy Sokolowski <44684409+aesokolowski@users.noreply.github.com>
2020-11-14 22:21:13 -0500

    fix: had my posts routes listed in the wrong order -- /{user} was before /create making /create inaccessible so I switched them around (should have known better since I know express.js)

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,6 +2,14 @@
 
 @section('content')
 <div class="container">
-    <img src="/storage/{{ $post->image }}" />
-</div>  {{-- container --}}
+    <div class="row">
+        <div class="col-8">
+            <img src="/storage/{{ $post->image }}" class="w-100" />
+        </div>
+
+        <div class="col-4">
+            <h3>{{ $post->user->userhandle }}</h3>
+        </div>
+    </div>
+</div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,10 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/p/{post}', [App\Http\Controllers\PostsController::class, 'show']);
 Route::get(
     '/p/create', [App\Http\Controllers\PostsController::class, 'create']
 );
+Route::get('/p/{post}', [App\Http\Controllers\PostsController::class, 'show']);
 Route::post(
     '/p', [App\Http\Controllers\PostsController::class, 'store']
 );
